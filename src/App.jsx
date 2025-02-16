5add662173f8813d7901c428a1a8913afd64a963
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-16 20:35:38 -0300

    Componente TelaB

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import TelaA from "./views/TelaA";
+import TelaB from "./views/TelaB";
 
 export default props =>{
     return(
         <SafeAreaView style={styles.container}>
             <TelaA/>
+            <TelaB/>
         </SafeAreaView>
     )
 }
