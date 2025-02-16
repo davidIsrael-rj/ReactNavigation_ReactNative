0dd3480f38d90d0936a56ce103c774322d507de0
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-16 20:38:00 -0300

    Coponente TelaC

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import TelaA from "./views/TelaA";
 import TelaB from "./views/TelaB";
+import TelaC from "./views/TelaC";
 
 export default props =>{
     return(
         <SafeAreaView style={styles.container}>
             <TelaA/>
             <TelaB/>
+            <TelaC/>
         </SafeAreaView>
     )
 }
---
+++ src/views/TelaC.jsx
@@ -0,0 +1,7 @@
+import TextoCentral from "../components/TextoCentral";
+
+export default prop =>(
+    <TextoCentral corFundo='#9932cd'>
+        Tela C
+    </TextoCentral>
+)
