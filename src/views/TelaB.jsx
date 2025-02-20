4cfd1629de05b96f79d2b6d46f84f20103bcb4c1
DavidIsrael <davidisrael.rj@gmail.com>
2025-02-19 21:07:28 -0300

    Botão na tela D para abrir o Drawer, e fechar depois de 3s

--- src/views/TelaB.jsx
+++ src/views/TelaB.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import TextoCentral from "../components/TextoCentral";
 
-export default prop =>(
+export default props =>(
     <TextoCentral corFundo='#3b82c4'>
         Tela B
     </TextoCentral>
---
+++ src/views/TelaD.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import TextoCentral from "../components/TextoCentral";
+import { Button, View } from "react-native";
+
+export default props => (
+    <View style={{ flex: 1 }}>
+        <View style={{ flexDirection: "row", justifyContent: "space-between"}}>
+            <Button
+                title='Abrir'
+                onPress={()=> {
+                    props.navigation.openDrawer()
+                    setTimeout(()=> {
+                        props.navigation.closeDrawer()
+                    }, 3000)
+                }}
+            />
+        </View>
+        <View style={{ flex: 1 }}>
+            <TextoCentral corFundo='#33fa72' corTexto='#000'>
+                Tela D
+            </TextoCentral>
+        </View>
+    </View>
+)
